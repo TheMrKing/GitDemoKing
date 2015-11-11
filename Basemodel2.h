eb349a413b9fb5450cb98dfe3955a6befecef216
wangtieshan <wangtieshan124@163.com>
2015-11-11 15:48:45 +0800

    commit By First

---
+++ Basemodel2.h
@@ -0,0 +1,13 @@
+//
+//  Basemodel2.h
+//  GitDemo
+//
+//  Created by KING on 15/11/11.
+//  Copyright © 2015年 KING. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Basemodel2 : NSObject
+
+@end
---
+++ Basemodel2.m
@@ -0,0 +1,13 @@
+//
+//  Basemodel2.m
+//  GitDemo
+//
+//  Created by KING on 15/11/11.
+//  Copyright © 2015年 KING. All rights reserved.
+//
+
+#import "Basemodel2.h"
+
+@implementation Basemodel2
+
+@end
