3bbf2acfd394be342f13ca11fed7308ca735219d
wangtieshan <wangtieshan124@163.com>
2015-11-11 15:45:23 +0800

    commit By Second

---
+++ GitDemo/BaseModel.h
@@ -0,0 +1,13 @@
+//
+//  BaseModel.h
+//  GitDemo
+//
+//  Created by KING on 15/11/11.
+//  Copyright © 2015年 KING. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BaseModel : NSObject
+
+@end
