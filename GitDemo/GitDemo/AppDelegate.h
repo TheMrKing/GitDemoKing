004ab07af799fd5f169f990272463ab355f8d7dc
wangtieshan <wangtieshan124@163.com>
2015-11-11 15:12:16 +0800

    first commit

---
+++ GitDemo/GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by KING on 15/11/11.
+//  Copyright © 2015年 KING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
